c62739e30ffff24ff7a80fb7a87d45117cda299f
Trần Tấn Dũng <85373307+tandung10x@users.noreply.github.com>
2021-12-30 10:33:53 +0700

    Update GetDataFile.py

--- GetDataFile.py
+++ GetDataFile.py
@@ -3,14 +3,14 @@ import os
 
 def DownloadFile():
     # delete the old database
-    if os.path.exists("CovidData.sqlite"):
-        os.remove("CovidData.sqlite")
+    if os.path.exists("data/CovidData.sqlite"):
+        os.remove("data/CovidData.sqlite")
 
     # download the csv file
     URL = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
     r = requests.get(URL, allow_redirects=False)
-    open('CovidData.csv', 'wb').write(r.content)
+    open('data/CovidData.csv', 'wb').write(r.content)
 
     # convert the csv file to sqlite file (already install csvs-to-sqlite)
-    os.system("csvs-to-sqlite CovidData.csv CovidData.sqlite")
+    os.system("csvs-to-sqlite data/CovidData.csv data/CovidData.sqlite")
     #time.sleep(3600)
